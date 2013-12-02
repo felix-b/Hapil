b127b0a1f41b0b62235fe1c35b858c72b2874934
lena-br <bleno4ka@gmail.com>
2013-12-02 16:55:25 +0200

    starting HappilFactory implementation

---
+++ Source/Happil/Fluent/HappilClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Happil.Fluent
+{
+    public class HappilClass: IClass
+    {
+        public IClass Inherits<TBase>(params Func<IHappilClassBody<TBase>, IMember>[] members)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClass Implements<TInterface>(params Func<IHappilClassBody<TInterface>, IMember>[] members)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type CreateType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Source/Happil/HappilFactory.cs
+++ Source/Happil/HappilFactory.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Happil.Fluent;
+using System.Reflection.Emit;
 
 namespace Happil
 {
 	public class HappilFactory
 	{
-		public IClass DefineClass(string fullName)
+	    private AssemblyBuilder m_AssemblyBuilder;
+
+	    public HappilFactory(string fullName, AssemblyBuilderAccess access = AssemblyBuilderAccess.RunAndSave)
+	    {
+	        m_AssemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(fullName), access);
+	    }
+
+	    //-----------------------------------------------------------------------------------------------------------------------------------------------------
+	    
+        public IClass DefineClass(string fullName)
 		{
+            
 			throw new NotImplementedException();
 		}
 	}
