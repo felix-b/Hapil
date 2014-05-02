b29371062e17bf23c5ff03e460b408d70abbf67c
felix-b <felixberman76@gmail.com>
2014-05-02 08:33:30 +0300

    Closures WIP 5 - testing operand visitor (task #45)

--- Source/Happil.UnitTests/Operands/OperandVisitorBaseTests.cs
+++ Source/Happil.UnitTests/Operands/OperandVisitorBaseTests.cs
@@ -13,7 +13,7 @@ namespace Happil.UnitTests.Operands
 	public class OperandVisitorBaseTests : ClassPerTestCaseFixtureBase
 	{
 		[Test]
-		public void CanReturnEmptyListIfNoOperands()
+		public void CanReturnEmptyListIfNoOperandsFound()
 		{
 			//-- Arrange
 
@@ -61,16 +61,84 @@ namespace Happil.UnitTests.Operands
 
 			//-- Act
 
-			var visitor = new TestVisitor(OperandKind.Argument, OperandKind.Local, OperandKind.Field, OperandKind.This);
+			var visitor = new TestVisitor();
 			method.AcceptVisitor(visitor);
 			
-			var visitedOperands = visitor.GetVisitedOperands();
+			var visitedOperandStrings = visitor.GetVisitedOperands().Select(op => op.ToString()).ToArray();
 
 			//-- Assert
 
-			CollectionAssert.AreEquivalent(
-				new[] { "Local0[String]", "Arg1[n]" },
-				visitedOperands.Select(op => op.ToString()).ToArray());
+			CollectionAssert.Contains(visitedOperandStrings, "Local0[String]");
+			CollectionAssert.Contains(visitedOperandStrings, "Arg1[n]");
+		}
+
+		//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+		[Test]
+		public void CanVisitThisAndFields()
+		{
+			//-- Arrange
+
+			Field<int> fieldOne;
+
+			DeriveClassFrom<object>()
+				.Field("m_One", out fieldOne)
+				.DefaultConstructor()
+				.ImplementInterface<AncestorRepository.IFewMethods>()
+				.Method<int, string>(intf => intf.Five).Implement((w, n) => {
+					fieldOne.Assign(n);
+					w.Return(fieldOne.Func<string>(x => x.ToString));
+				})
+				.AllMethods().Throw<NotImplementedException>()
+				.Flush();
+
+			var method = base.Class.GetAllMembers().OfType<MethodMember>().Single(m => m.Name == "Five");
+			method.Write();
+
+			//-- Act
+
+			var visitor = new TestVisitor();
+			method.AcceptVisitor(visitor);
+
+			var visitedOperandStrings = visitor.GetVisitedOperands().Select(op => op.ToString()).ToArray();
+
+			//-- Assert
+
+			CollectionAssert.Contains(visitedOperandStrings, "this");
+			CollectionAssert.Contains(visitedOperandStrings, "this.Field[m_One]");
+		}
+
+		//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+		[Test]
+		public void CanFilterOperands()
+		{
+			//-- Arrange
+
+			DeriveClassFrom<object>()
+				.DefaultConstructor()
+				.ImplementInterface<AncestorRepository.IFewMethods>()
+				.Method<int, string>(intf => intf.Five).Implement((w, n) => {
+					var s = w.Local<string>();
+					s.Assign(n.Func<string>(x => x.ToString));
+					w.Return(s);
+				})
+				.AllMethods().Throw<NotImplementedException>()
+				.Flush();
+
+			var method = base.Class.GetAllMembers().OfType<MethodMember>().Single(m => m.Name == "Five");
+			method.Write();
+
+			//-- Act
+
+			var visitor = new TestVisitor(OperandKind.Argument);
+			method.AcceptVisitor(visitor);
+
+			var visitedOperandStrings = visitor.GetVisitedOperands().Select(op => op.ToString()).ToArray();
+
+			//-- Assert
+
+			Assert.That(visitedOperandStrings, Is.EqualTo(new[] { "Arg1[n]" }));
 		}
 
 		//-----------------------------------------------------------------------------------------------------------------------------------------------------
