2ca043f65e721911020c6321ac70f013ce8bdedf
felix-b <felixberman76@gmail.com>
2014-10-31 15:49:14 +0200

    Initial commit of samples solution

---
+++ Samples/Happil.Samples.Logging.UnitTests/DummyTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Happil.Samples.Logging.UnitTests
+{
+	[TestFixture]
+    public class DummyTests
+    {
+		[Test]
+		public void TestOne()
+		{
+		}
+    }
+}
