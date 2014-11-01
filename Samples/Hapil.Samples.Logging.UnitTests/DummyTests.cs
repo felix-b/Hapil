199b0afacb3c661577989f2efd0561d45b951a09
felix-b <felixberman76@gmail.com>
2014-11-01 06:32:37 +0200

    CI Push testing 2

--- Samples/Hapil.Samples.Logging.UnitTests/DummyTests.cs
+++ Samples/Hapil.Samples.Logging.UnitTests/DummyTests.cs
@@ -14,5 +14,15 @@ namespace Hapil.Samples.Logging.UnitTests
 		{
 			Assert.That(10, Is.EqualTo(10), "10 doesn't equal to 10?");
 		}
-    }
+		[Test]
+		public void TestTwo()
+		{
+			Assert.That(20, Is.EqualTo(20), "20 doesn't equal to 20?");
+		}
+		[Test]
+		public void TestThree()
+		{
+			Assert.That(30, Is.EqualTo(30), "30 doesn't equal to 30?");
+		}
+	}
 }
